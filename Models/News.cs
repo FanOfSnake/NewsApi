705c72262d1e5e13d453698ebc359b1121333887
FanOfSnake <shaukatk@list.ru>
2021-04-11 13:22:17 +0300

    Перенес xml комменты в дтошку новостей

--- Models/News.cs
+++ Models/News.cs
@@ -9,9 +9,7 @@ using NewsApi.Models.DTO;
 
 namespace NewsApi.Models
 {
-    /// <summary>
-    /// News' entity
-    /// </summary>
+    
     public class News
     {
         public News ()
@@ -31,33 +29,12 @@ namespace NewsApi.Models
             this.CategoriesId = news.CategoriesId;
         }
         public int Id { get; set; }
-        /// <summary>
-        /// Url for the New's img
-        /// </summary>
         public string Img { get; set; }
-        /// <summary>
-        /// The name of the news
-        /// </summary>
         public string Name { get; set; }
-        /// <summary>
-        /// The short description for the news
-        /// </summary>
         public string ShortDesc { get; set; }
-        /// <summary>
-        /// The text of the news
-        /// </summary>
         public string Text { get; set; }
-        /// <summary>
-        /// The publication time of the news
-        /// </summary>
         public DateTime TimePublication { get; set; }
-        /// <summary>
-        /// The new's categories
-        /// </summary>
         public List<Category> Categories { get; set; } = new List<Category>();
-        /// <summary>
-        /// The new's comments
-        /// </summary>
         public List<Comment> Comments { get; set; } = new List<Comment>();
         public List<int> CategoriesId { get; set; }
         public List<int> CommentsId { get; set; }
