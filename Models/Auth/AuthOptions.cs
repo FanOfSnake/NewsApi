662218bb4af4628c4def154ed01b2ac6bc671464
FanOfSnake <shaukatk@list.ru>
2021-04-11 13:54:58 +0300

    начал добавлять аутентификацию с помощью jwt: добавил пакет jwt bearer а также создал папку для сущностей авторизации: пока что там находятся user.cs и AuthOptions.cs

---
+++ Models/Auth/AuthOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+namespace NewsApi.Models.Auth
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer";
+        public const string AUDIENCE = "MyAuthClient";
+        private const string KEY = "mu_secret_key!123";
+        public const int LIFETIME = 1; // 1 minute
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+
+    }
+}
--- Models/Auth/User.cs
+++ Models/Auth/User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NewsApi.Models
+namespace NewsApi.Models.Auth
 {
     public class User
     {
